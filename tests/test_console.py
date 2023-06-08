ee1c66eb46f004feb02eef30930fe43e4158f793
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-08 04:09:24 +0100

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """Test console for expected behaviour and documentation"""
+
 import unittest
+import json
+import pep8
+from io import StringIO
+from models.engine.file_storage import FileStorage
+import os
