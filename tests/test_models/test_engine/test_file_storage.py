e1c923dcfe081d7829b66db349d1560cd42d7560
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-08 03:55:26 +0100

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Test required behaviour and documentations"""
+"""Test for file Storage documentations"""
+
 import unittest
 import json
 import os
 import pep8
 import unittest
 import inspect
-
-if __name__== '__main__':
-    unittest.main()
