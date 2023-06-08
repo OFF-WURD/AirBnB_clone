fccd5ae41f715e39de4b7a9d011273941bde463f
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-08 03:41:07 +0100

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """Test required behaviour and documentations"""
 import unittest
+import json
+import os
+import pep8
+import unittest
+import inspect
+from models.engine import file_storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 if __name__== '__main__':
     unittest.main()
