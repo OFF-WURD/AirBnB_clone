15451c7c83530945a0cedd8936370748ccfefd85
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-11 03:44:15 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,18 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    '''A Baseclass for all other class'''
-    
     def __init__(self):
-        '''constructor'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        '''return [<class name>] (<self.id>) <self.__dict__>'''
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        '''updates the public instance attribute updated_at with the current datetime'''
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        '''returns a dictionary containing all keys/values of __dict__ of the instance'''
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
