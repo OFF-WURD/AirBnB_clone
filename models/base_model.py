5eb483cb4e38b8e02937dcabfeed639c88872471
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-11 03:35:37 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,19 @@ from datetime import datetime
 class BaseModel:
     '''A Baseclass for all other class'''
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''constructor'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in['created_at','updated_at']:
+                        #convert string to datetime object
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''return [<class name>] (<self.id>) <self.__dict__>'''
