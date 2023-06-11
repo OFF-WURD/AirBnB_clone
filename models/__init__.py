fb64a74e4ad277abda949b70ae756a1c7b69a2ba
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-11 03:55:46 +0100

    __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from models.engine.file_storage import FileStorage()
+'''to create a unique FileStorage instance for your application'''
+storage = FileStorage()
+storage.reload()
