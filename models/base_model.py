0836275853e6b7b307a5197a51caf56023d623b8
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-11 03:18:31 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-from datetime import datetime
 import uuid
+from datetime import datetime
 
-class BaseModel():
-    ''' a Baseclass for all other model'''
-
+class BaseModel:
     def __init__(self):
-        '''constructor'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    
 
     def __str__(self):
-        ''' [<class name>] (<self.id>) <self.__dict__>'''
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        ''' updates the public instance attribute 
-        updated_at with the current datetime '''  
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        '''  returns a dictionary containing all 
-        keys/values of __dict__ of the instance'''
-        
-        obj = self.__dict__.copy() #copying instance of a class
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
         
         #adding class name to the dictionary
         obj[__class__] = type(self).__name__
