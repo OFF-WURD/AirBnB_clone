2aa2d9b54dbe725f32b581d196d8387a58e59d3c
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-11 03:58:59 +0100

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import storage
 
 class BaseModel:
     '''A Baseclass for all other class'''
@@ -18,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         '''return [<class name>] (<self.id>) <self.__dict__>'''
@@ -26,6 +28,7 @@ class BaseModel:
     def save(self):
         '''updates the public instance attribute updated_at with the current datetime'''
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values of __dict__ of the instance'''
