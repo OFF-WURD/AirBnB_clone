7990679dba91bc8cc111668826999e9e7ceebad5
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-12 03:13:48 +0100

    console.py

--- console.py
+++ console.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 import cmd
-class HBNBCommand(cmd.Cmd):
-    '''console.py that contains the entry point of the command interpreter:'''
-    prompt = '(hbnb)'
 
-    def do_quit(self, args):
-        '''Quit the program'''
-        return True #returning True will exit the loop
-    
-    def do_EOF(self, args):
-        '''handle the end-of-file character'''
-        print() #print a newline before exiting
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Exit the program"""
         return True
-    
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        return True
+
     def do_help(self, arg):
         """List available commands with 'help' or detailed help with 'help cmd'."""
         super().do_help(arg)
-        
+
     def emptyline(self):
-        '''override emptyline not to do anything'''
+        """Do nothing when an empty line is entered"""
         pass
 
 if __name__ == '__main__':
