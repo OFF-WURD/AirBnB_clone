5b2671e6d542a98ae34995761ceb06636a81bcbe
OFF-WURD <107750709+OFF-WURD@users.noreply.github.com>
2023-06-27 03:23:35 +0100

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+'''Amenity class that inherit from BaseModel'''
+class Amenity(BaseModel)
+    name=""
+    def __init__(self,*args, **kwargs):
+        '''initialization'''
+        super().__init__(*args, **kwargs)
